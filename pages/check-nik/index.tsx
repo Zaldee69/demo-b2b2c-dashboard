c485348325536b5da41b68885b33225b379736cd
Muhammad Adam Canrayneldy <muhammad.adam@tilaka.id>
2022-06-22 13:27:31 +0000

    [TDP-2153] [FE] membuat halaman popup menampilkan hasil check NIK

--- pages/check-nik/index.tsx
+++ pages/check-nik/index.tsx
@@ -21,6 +21,14 @@ const CheckNIK: NextPage = () => {
 
     // hit api with form state data
     alert(JSON.stringify(form, null, 4));
+
+    // success ?
+    // openModalCheckNIKResult();
+    toggleModalCheckNIKResult();
+  };
+
+  const toggleModalCheckNIKResult = (): void => {
+    document.getElementById("modalCheckNIKResult")?.click();
   };
 
   return (
@@ -88,6 +96,70 @@ const CheckNIK: NextPage = () => {
             </form>
           </div>
         </div>
+
+        <input
+          type="checkbox"
+          id="modalCheckNIKResult"
+          className="modal-toggle"
+        />
+        <label className="modal cursor-pointer" htmlFor="modalCheckNIKResult">
+          <label
+            className="modal-box rounded max-w-3xl px-6 pt-4 pb-6 bg-white"
+            htmlFor=""
+          >
+            <div className="flex items-start md:items-center px-4 py-5 flex-wrap flex-col md:flex-row ">
+              <p className="text-sm text-neutral200">Request ID</p>
+              <div className="text-sm text-neutral800 bg-neutral40 px-2.5 py-0.5 ml-0 md:ml-3 mt-3 md:mt-0">
+                de836b20-2b27-4180-ab5b-d5703edb5717
+              </div>
+              <div className="bg-registerColor px-3 py-0.5 text-neutral800 rounded-xl ml-0 md:ml-3 mt-3 md:mt-0">
+                Daftar
+              </div>
+            </div>
+            <div className="bg-neutral20 px-4 sm:px-12 md:px-32 py-4">
+              <p className="text-lg sm:text-xl md:text-2xl font-normal text-center text-neutral800">
+                Halaman <span className="italic">Customer Consent</span>
+              </p>
+              <div className="mt-3 text-center">
+                <Image
+                  src="/illustration/CustomerConsent.svg"
+                  width="151px"
+                  height="151px"
+                />
+              </div>
+              <p className="mt-3 font-normal text-neutral800 text-sm">
+                Dengan melakukan Aktivasi Akun Tilaka berarti Anda setuju untuk
+                membagikan informasi kepada Tilaka Nusa Teknologi sebagai
+                partner tanda tangan digital Dana Bagus.
+              </p>
+              <label
+                className="mt-3 label cursor-pointer"
+                htmlFor="checkNIKResultAgree"
+              >
+                <input
+                  id="checkNIKResultAgree"
+                  type="checkbox"
+                  className="checkbox checkbox-primary"
+                />
+                <p className="ml-4 font-normal text-neutral200 text-xs">
+                  Saya menyatakan bahwa data yang saya isi adalah benar dan
+                  bertanggung jawab atas keaslian data.
+                </p>
+              </label>
+            </div>
+            <div className="flex justify-end mt-5">
+              <button
+                onClick={toggleModalCheckNIKResult}
+                className="bg-white hover:opacity-50 text-blue400 px-6 py-2.5 rounded hover:cursor-pointer"
+              >
+                Batal
+              </button>
+              <button className="bg-blue400 hover:opacity-50 text-white px-6 py-2.5 rounded hover:cursor-pointer">
+                Lanjut
+              </button>
+            </div>
+          </label>
+        </label>
       </div>
     </>
   );
